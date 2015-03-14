6092c41032a17c86b0a3f8dbb9d5c7a4635ac73f
fatcloud <victor19871226@hotmail.com>
2015-03-15 05:50:23 +0800

    loop instead of recursion, almost no difference in memory and speed

--- Easy/fibonacci_series.py
+++ Easy/fibonacci_series.py
@@ -1,19 +1,23 @@
 import sys
+test_cases = open(sys.argv[1], 'r')
+
+tests = []
+max_test = 0
+for test in test_cases:
+    if test != '\n':
+        tmp = int(test)
+        if tmp > max_test:
+            max_test = tmp
+        tests.append(tmp)
+
+test_cases.close()
 
 fibos = [0, 1]
 
+for i in range(2, max_test + 1):
+    fibos.append(fibos[i-1] + fibos[i - 2])
 
-def f(n):
-    n = int(n)
-    if n > len(fibos) - 1:
-        fibos.append(f(n - 1) + f(n - 2))
-    return fibos[n]
+for t in tests:
+    print fibos[t]
 
 
-test_cases = open(sys.argv[1], 'r')
-
-for test in test_cases:
-    if test != '\n':
-        print f(test)
-
-test_cases.close()
