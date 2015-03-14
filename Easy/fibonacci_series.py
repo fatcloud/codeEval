33fed1ff8e77d142f9b7745ef528c8e2aa5f4fba
fatcloud <victor19871226@hotmail.com>
2015-03-15 06:19:36 +0800

    save memory by not saving a lot sequence

--- Easy/fibonacci_series.py
+++ Easy/fibonacci_series.py
@@ -12,10 +12,19 @@ for test in test_cases:
 
 test_cases.close()
 
-fibos = [0, 1]
+fibos = {0: 0, 1: 1}
 
-for i in range(2, max_test + 1):
-    fibos.append(fibos[i-1] + fibos[i - 2])
+f_1 = 0
+f_2 = 1
+i = 2
+while i <= max_test:
+    f = f_1 + f_2
+    if i in tests:
+        fibos[i] = f
+    f_1 = f_2
+    f_2 = f
+    i += 1
+
 
 for t in tests:
     print fibos[t]
